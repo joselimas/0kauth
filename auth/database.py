fc1bc2853af86062222ef8a1474ab9a928ebf4a1
jllis <jose.limas@gmail.com>
2017-08-10 13:42:05 +0000

    fix db funcs

--- auth/database.py
+++ auth/database.py
@@ -4,4 +4,4 @@ def get_permitted_users():
     with sqlite3.connect('db/auth.db') as conn:
         c=conn.cursor()
         query=c.execute("select * from users where username='get_flag';")
-        return {user:[int(salt,16),int(secret,16)] for salt,user,secret in query.fetchall()}
+        return {user:[int(salt,16),int(secret,16)] for salt,user,secret,first_flag in query.fetchall()}
